b141393adf5044979264eb9e774fcb1d660b5a71
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2025-06-29 18:21:17 +0200

    Added CalculateNumber function
    - cons: only counts up to 3 and only on some fingers...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy
+import math
 
 class HandTracker():
     def __init__(self):
@@ -9,10 +10,11 @@ class HandTracker():
         self.mp_draw = mp.solutions.drawing_utils
         self.mp_draw_style = mp.solutions.drawing_styles
 
+        self.windowSize = (640, 480)
+
         self.tracking(self.camera_capture, self.mp_draw, self.mp_draw_style)
     
-     
-
+    # Tracking
     def tracking(self, cam, draw, style):
         with self.mp_hands.Hands(min_detection_confidence=0.75) as hands:
             while True:
@@ -35,9 +37,12 @@ class HandTracker():
                                             self.mp_hands.HAND_CONNECTIONS,
                                             style.get_default_hand_landmarks_style(),
                                             style.get_default_hand_connections_style())
-                # self.hand_tracking(img_RGB, captured_img, self.mp_draw, self.mp_draw_style)
+                        self.CalculateNumber(hand_landmarks)
+                # self.hand_tracking(img_RGB, captured_img, self.mp_draw, self.mp_draw_style) # <-- What's that?
+                # https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker # <-- Why not
                 # Outputs Inverted Image's to Window (as live video)
                 cv2.imshow('Output', cv2.flip(img_RGB, 1))
+                cv2.resizeWindow('Output', self.windowSize)
                 # Closes App Once "X" Has Been Hit
                 key_code = cv2.waitKey(1)
                 if cv2.getWindowProperty('Output', cv2.WND_PROP_VISIBLE) < 1:
@@ -45,6 +50,27 @@ class HandTracker():
             # Stops Camera Usage and Closes Output Windows
             cam.release()
             cv2.destroyAllWindows()
+    
+    # Calculate number show by fingers
+    def CalculateNumber(self, _handLandmarks):
+        count = 0
+
+        _tipsIndex = [4, 8, 12, 16, 20]
+        _palmIndex = _handLandmarks.landmark[0]
+
+        _middleIndex = _handLandmarks.landmark[9] # Middle finger knuckle as baseline
+        _referenceDistance = math.hypot(_palmIndex.x - _middleIndex.x, _palmIndex.y - _middleIndex.y)
+
+        for n, i in enumerate(_tipsIndex):
+            _tipLandmark = _handLandmarks.landmark[i]
+            _tipDistance = math.hypot(_palmIndex.x - _tipLandmark.x, _palmIndex.y - _tipLandmark.y)
+
+            _threshold = 1.3 if n == 0 else 1.8
+
+            if _tipDistance > _referenceDistance * _threshold:
+                count += 1
+
+        print(count)
 
 if __name__=="__main__":
     app = HandTracker()
